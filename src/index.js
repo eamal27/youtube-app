6380684e091be64c88147568d546bb59444b6b55
eamal27 <elias.amal27@gmail.com>
2021-04-07 12:45:15 -0400

    .env added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyC_X841ubIPHSL3ntReVVbkQHn9y92QLHM';
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 
 class App extends Component {
